4ffaace4a1eec7c756fd61ddec07de6a88f156f1
Eirik <eirikmyklebost+github@gmail.com>
2021-09-27 15:05:37 +0200

    Added tests

--- .docker/docker-entrypoint.sh
+++ .docker/docker-entrypoint.sh
@@ -5,8 +5,10 @@ elif [ "$1" = 'validator' ]; then
     exec java -Xmx2048m -jar validator_cli.jar ${@:2}
 elif [ "$1" = 'publisher' ]; then
     exec java -Xmx2048m -jar publisher.jar ${@:2}
+elif [ "$1" = 'test' ]; then
+    exec java -Xmx2048m -jar junit_cli.jar -cp engine.jar:validator_cli.jar ${@:2}
 else
     echo "ERROR: Unknown argument: $1"
-    echo "Valid arguments are [sushi, validator, publisher]"
+    echo "Valid arguments are [sushi, validator, publisher, test]"
     exit 1
 fi
